63bda54bb5801ba610d0401c9264e96453a64fd1
Zawadzki Kuba <58346333+kubeusz04@users.noreply.github.com>
2026-07-18 01:52:07 +0200

    Add files via upload

---
+++ towers/pl-4.js
@@ -0,0 +1,32 @@
+window.TOWER_LEVELS = [
+  {
+    foundation: 'To',
+    slots: ['jest', 'kot'],
+    en: 'This is a cat',
+    extra: ['ten', 'ta', 'tego', 'tej']
+  },
+  {
+    foundation: 'Nie',
+    slots: ['lubię', 'tego'],
+    en: "I don't like this",
+    extra: ['ten', 'ta', 'tej', 'tę']
+  },
+  {
+    foundation: 'Co',
+    slots: ['to'],
+    en: "What's that?",
+    extra: ['ten', 'ta', 'tego', 'tej']
+  },
+  {
+    foundation: 'ta',
+    slots: ['dziewczyna'],
+    en: 'this girl',
+    extra: ['ten', 'to', 'tego', 'tej']
+  },
+  {
+    foundation: 'Jestem',
+    slots: ['najszczęśliwszy', 'na', 'świecie'],
+    en: "I'm the happiest in the world",
+    extra: ['tego', 'tej', 'ten', 'ta']
+  }
+];
